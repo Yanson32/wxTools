86f5e5c3590bc26678b414f2df2c9397c181afe9
Yanson32 <wlarson30@gmail.com>
2023-12-29 15:42:08 -0500

    Added a script to build the library with gcc

---
+++ Devel/CMake/gcc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cmake -S . -B ./build/gcc/debug -DCMAKE_BUILD_TYPE:STRING="Debug" -DCMAKE_CXX_FLAGS_DEBUG:STRING="-g -Wall -Wextra" -DAWM_WX_VERSION="3.2.4"
+cmake -S . -B ./build/gcc/release -DCMAKE_BUILD_TYPE:STIRNG="Release" 
+
