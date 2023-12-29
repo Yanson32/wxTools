0bf773a6fe46eb96b690e4610ae9fcac8075e8ba
Yanson32 <wlarson30@gmail.com>
2023-12-29 15:41:16 -0500

    Added some basic cmake helper files

---
+++ cmake/modules/debug.cmake
@@ -0,0 +1,13 @@
+macro(debug_report MSG)
+    message(VERBOSE "${MSG}")
+    file(APPEND "${CMAKE_BINARY_DIR}/DebugReport.txt" "${MSG}\n")
+endmacro()
+
+
+macro(debug_init)
+    file(REMOVE "${CMAKE_BINARY_DIR}/DebugReport.txt" "${MSG}\n")
+    debug_report("PROJECT_NAME = ${PROJECT_NAME}")
+    debug_report("CMAKE_PROJECT_VERSION + ${CMAKE_PROJECT_VERSION}")
+    debug_report("Source file = ${CMAKE_CURRENT_LIST_FILE}")
+    debug_report("AWM_WX_VERSION = ${AWM_WX_VERSION}")
+endmacro()
