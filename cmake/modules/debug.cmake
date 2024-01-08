7b48e0209449005ffb147dddc7d80dd3460d60c3
Yanson32 <wlarson30@gmail.com>
2024-01-07 21:25:38 -0500

    Added some functions for displaing target properties

--- cmake/modules/debug.cmake
+++ cmake/modules/debug.cmake
@@ -10,3 +10,23 @@ macro(debug_init)
     debug_report("CMAKE_PROJECT_VERSION = ${CMAKE_PROJECT_VERSION}")
     debug_report("Source file = ${CMAKE_CURRENT_LIST_FILE}")
 endmacro()
+
+#Print a target property
+function(debug_print_property PARAM_TARGET PARAM_PROPERTY)
+    get_target_property(tmp_PROPERTY "${PARAM_TARGET}" "${PARAM_PROPERTY}")
+    foreach(VAR ${tmp_PROPERTY})
+        debug_report("TARGET = ${PARAM_TARGET} PROPERTY = ${PARAM_PROPERTY}  DATA = ${VAR}")
+    endforeach()
+endfunction()
+
+
+#Display all sources for a target
+function(debug_print_sources PARAM_TARGET)
+    debug_print_property("${PARAM_TARGET}" "SOURCES")
+endfunction()
+
+#Display a targets include directories
+function(debug_print_include_directories PARAM_TARGET)
+    debug_print_property("${PARAM_TARGET}" "INCLUDE_DIRECTORIES") 
+endfunction()
+
