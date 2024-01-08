876f333eb47abc48d4c5cf4928c4742d7c260efe
Yanson32 <wlarson30@gmail.com>
2024-01-08 09:48:18 -0500

    Bugfix: Variable named incorrectly for the current project

--- Devel/CMake/gcc.sh
+++ Devel/CMake/gcc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cmake -S . -B ./build/gcc/debug -DCMAKE_BUILD_TYPE:STRING="Debug" -DCMAKE_CXX_FLAGS_DEBUG:STRING="-g -Wall -Wextra" -DAWM_WX_VERSION="3.2.4" -DWXT_EXAMPLES="True"
+cmake -S . -B ./build/gcc/debug -DCMAKE_BUILD_TYPE:STRING="Debug" -DCMAKE_CXX_FLAGS_DEBUG:STRING="-g -Wall -Wextra" -DWXT_WX_VERSION="3.2.4" -DWXT_EXAMPLES="True"
 cmake -S . -B ./build/gcc/release -DCMAKE_BUILD_TYPE:STIRNG="Release" 
 
