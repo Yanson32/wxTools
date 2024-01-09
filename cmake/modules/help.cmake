bb299f77afb0983f76f65c2fc9f8ff66814c865c
Yanson32 <wlarson30@gmail.com>
2024-01-09 03:24:04 -0500

    Update the help text

--- cmake/modules/help.cmake
+++ cmake/modules/help.cmake
@@ -1,4 +1,5 @@
 function(display_help_text)
+    message("BUILD_SHARED_LIBS  Set this variable to True for dynamic libraries and False for staic libraries")
     message("WXT_EXAMPLES       Set to true to build examples")
     message("WXT_WX_VERSION     Set to the desired version of the wxWidgets library")
 endfunction()
